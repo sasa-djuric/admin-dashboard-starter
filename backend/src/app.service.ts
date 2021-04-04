9a057b18c9dca47430ff1e5ddb3c5021d052e091
Saša Đurić <djsasa.mail@gmail.com>
2021-04-04 18:44:35 +0200

    Checking for existing admin role

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,42 +1,54 @@
-import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { InjectRepository } from '@nestjs/typeorm';
-import { AppConfig } from 'src/config/app';
-import { Repository } from 'typeorm';
-import { Role } from './modules/roles/role.entity';
-import { UserRepository } from './modules/users/user.repository';
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { InjectRepository } from "@nestjs/typeorm";
+import { AppConfig } from "src/config/app";
+import { Repository } from "typeorm";
+import { Role } from "./modules/roles/role.entity";
+import { UserRepository } from "./modules/users/user.repository";
 
 @Injectable()
 export class AppService {
 	constructor(
-		@InjectRepository(Role) private readonly roleRepository: Repository<Role>,
+		@InjectRepository(Role)
+		private readonly roleRepository: Repository<Role>,
 		private readonly userRepository: UserRepository,
 		private readonly configService: ConfigService
 	) {}
 
 	private async isFirstRun() {
-		return this.userRepository.count().then(result => !result);
+		return this.userRepository.count().then((result) => !result);
 	}
 
 	public async handleFirstRun() {
-		const appConfig = this.configService.get<AppConfig>('app');
+		const appConfig = this.configService.get<AppConfig>("app");
 		const isFirstRun = await this.isFirstRun();
 
 		if (!isFirstRun) {
 			return;
 		}
 
-		const entities = ['roles', 'users'];
-		const permissions = ['create', 'read', 'update', 'delete'];
-		const allPermissions = entities.flatMap(entity => permissions.map(permission => `${entity}:${permission}`));
-
-		const role = this.roleRepository.create({
-			name: 'Admin',
-			isActive: true,
-			permissions: allPermissions
+		const entities = ["roles", "users"];
+		const permissions = ["create", "read", "update", "delete"];
+		const allPermissions = entities.flatMap((entity) =>
+			permissions.map((permission) => `${entity}:${permission}`)
+		);
+		const existingRole = await this.roleRepository.findOne({
+			name: "Admin",
 		});
 
-		await this.roleRepository.insert(role);
+		let role: Role;
+
+		if (existingRole) {
+			role = existingRole;
+		} else {
+			role = this.roleRepository.create({
+				name: "Admin",
+				isActive: true,
+				permissions: allPermissions,
+			});
+
+			await this.roleRepository.insert(role);
+		}
 
 		return this.userRepository.insert({
 			email: appConfig.userEmail,
@@ -44,7 +56,7 @@ export class AppService {
 			password: appConfig.userPassword,
 			roleId: role.id,
 			isActive: true,
-			isActivated: true
+			isActivated: true,
 		});
 	}
 }
