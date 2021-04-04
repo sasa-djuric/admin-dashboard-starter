819d8f47059b235f077d2c66bfff51f80e7aed40
Saša Đurić <djsasa.mail@gmail.com>
2021-04-04 20:18:50 +0200

    Adjusted dashboard view

--- frontend/apps/dashboard/src/apps/users/views/main/main.tsx
+++ frontend/apps/dashboard/src/apps/users/views/main/main.tsx
@@ -33,133 +33,128 @@ import settings from '../../';
 import { paginationConfig } from '../../../../config/pagination';
 
 const columns = ({ onEdit, onDelete }: any): ColumnsType<TableRow> => [
-  {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name',
-    sorter: (a, b) => 0,
-  },
-  {
-    title: 'Status',
-    dataIndex: 'status',
-    key: 'status',
-    sorter: (a, b) => 0,
-  },
-  {
-    title: 'Action',
-    key: 'action',
-    render(text, row, index) {
-      return (
-        <Space size="middle">
-          <Protected permissions={[UsersPermissions.Update]}>
-            <Button
-              icon={<EditOutlined onClick={() => onEdit(row, index)} />}
-            />
-          </Protected>
-          <Protected permissions={[UsersPermissions.Delete]}>
-            <Button
-              icon={<DeleteOutlined />}
-              danger={true}
-              onClick={() => onDelete(row, index)}
-            />
-          </Protected>
-        </Space>
-      );
-    },
-  },
+	{
+		title: 'Name',
+		dataIndex: 'name',
+		key: 'name',
+		sorter: (a, b) => 0
+	},
+	{
+		title: 'Status',
+		dataIndex: 'status',
+		key: 'status',
+		sorter: (a, b) => 0
+	},
+	{
+		title: 'Action',
+		key: 'action',
+		render(text, row, index) {
+			return (
+				<Space size='middle'>
+					<Protected permissions={[UsersPermissions.Update]}>
+						<Button icon={<EditOutlined onClick={() => onEdit(row, index)} />} />
+					</Protected>
+					<Protected permissions={[UsersPermissions.Delete]}>
+						<Button
+							icon={<DeleteOutlined />}
+							danger={true}
+							onClick={() => onDelete(row, index)}
+						/>
+					</Protected>
+				</Space>
+			);
+		}
+	}
 ];
 
 const mapData = (data: Array<User>): Array<TableRow> =>
-  data.map((item: User) => ({
-    ...item,
-    key: item.id,
-    status: item.isActive ? 'Active' : 'Inactive',
-  }));
+	data.map((item: User) => ({
+		...item,
+		key: item.id,
+		status: item.isActive ? 'Active' : 'Inactive'
+	}));
 
 interface UsersMainViewProps extends RouteComponentProps<{ id?: string }> {}
 
-const UsersMainView: FunctionComponent<UsersMainViewProps> = ({
-  history,
-  match,
-}) => {
-  const id = match.params.id ? +match.params.id : undefined;
-  const isMaster = typeof id !== 'number';
-  const [search, setSearch] = useState('');
-  const [page, setPage] = useState(1);
-  const [sort, setSort] = useState<Sorting>();
-  const { data: users, isLoading } = useQuery(
-    usersService.queries.getAllPaginated({
-      id,
-      search,
-      offset: (page - 1) * 5,
-      limit: paginationConfig.limit,
-      ...sort,
-    })
-  );
+const UsersMainView: FunctionComponent<UsersMainViewProps> = ({ history, match }) => {
+	const id = match.params.id ? +match.params.id : undefined;
+	const isMaster = typeof id !== 'number';
+	const [search, setSearch] = useState('');
+	const [page, setPage] = useState(1);
+	const [sort, setSort] = useState<Sorting>();
+	const { data: users, isLoading } = useQuery(
+		usersService.queries.getAllPaginated({
+			id,
+			search,
+			offset: (page - 1) * 5,
+			limit: paginationConfig.limit,
+			...sort
+		})
+	);
 
-  function onNew() {
-    history.push(`/${settings.name}${!isMaster ? `/${id}` : ''}/new`);
-  }
+	function onNew() {
+		history.push(`/${settings.name}${!isMaster ? `/${id}` : ''}/new`);
+	}
 
-  function onEdit(row: TableRow, index: number) {
-    history.push(`/${settings.name}/${row.id}/edit`);
-  }
+	function onEdit(row: TableRow, index: number) {
+		history.push(`/${settings.name}/${row.id}/edit`);
+	}
 
-  function onDelete(row: TableRow, index: number) {
-    confirm.delete({
-      title: 'Do you want to delete this item?',
-      onOk() {
-        return usersService.remove(row.id);
-      },
-    });
-  }
+	function onDelete(row: TableRow, index: number) {
+		confirm.delete({
+			title: 'Do you want to delete this item?',
+			onOk() {
+				return usersService.remove(row.id);
+			}
+		});
+	}
 
-  function onSearchChange(e: React.ChangeEvent<HTMLInputElement>) {
-    setSearch(e.target.value);
-  }
+	function onSearchChange(e: React.ChangeEvent<HTMLInputElement>) {
+		setSearch(e.target.value);
+	}
 
-  return (
-    <div>
-      <PageHeader
-        ghost={false}
-        title="Users"
-        subTitle="List of users"
-        extra={[
-          <Protected permissions={[UsersPermissions.Create]}>
-            <Button key="new" onClick={onNew}>
-              Add New
-            </Button>
-          </Protected>,
-        ]}
-        style={{ marginBottom: '24px' }}
-        onBack={id ? () => history.goBack() : undefined}
-      />
+	return (
+		<div>
+			<PageHeader
+				ghost={false}
+				title='Users'
+				subTitle='List of users'
+				extra={[
+					<Protected permissions={[UsersPermissions.Create]}>
+						<Button key='new' onClick={onNew}>
+							Add New
+						</Button>
+					</Protected>
+				]}
+				style={{ marginBottom: '24px' }}
+				onBack={id ? () => history.goBack() : undefined}
+			/>
 
-      <Card>
-        <Row align="middle">
-          <Search
-            style={{ display: 'flex', width: '250px', marginLeft: 'auto' }}
-            placeholder="Search"
-            onChange={debounce(onSearchChange, 500)}
-          />
-        </Row>
-        <Divider />
-        <Table
-          columns={columns({ onEdit, onDelete })}
-          dataSource={users?.data ? mapData(users.data) : []}
-          style={{ overflowX: 'auto' }}
-          loading={isLoading}
-          pagination={{
-            defaultPageSize: 5,
-            total: users?.metadata?.total,
-            current: page,
-            onChange: setPage,
-          }}
-          onChange={handleTableChangeSort(setSort)}
-        />
-      </Card>
-    </div>
-  );
+			<Card>
+				<Row align='middle'>
+					<Search
+						style={{ display: 'flex', width: '250px', marginLeft: 'auto' }}
+						placeholder='Search'
+						onChange={debounce(onSearchChange, 500)}
+					/>
+				</Row>
+				<Divider />
+				<Table
+					columns={columns({ onEdit, onDelete })}
+					dataSource={users?.data ? mapData(users.data) : []}
+					style={{ overflowX: 'auto' }}
+					loading={isLoading}
+					pagination={{
+						defaultPageSize: 5,
+						total: users?.metadata?.total,
+						current: page,
+						onChange: setPage
+					}}
+					onChange={handleTableChangeSort(setSort)}
+				/>
+			</Card>
+		</div>
+	);
 };
 
 export default UsersMainView;
--- frontend/apps/dashboard/src/views/dashboard/dashboard.tsx
+++ frontend/apps/dashboard/src/views/dashboard/dashboard.tsx
@@ -1,28 +1,152 @@
 // Components
-import { Card, Col, Row, Statistic } from 'antd';
+import { Card, Col, Divider, Row, Statistic } from 'antd';
+import { AreaChart, Area, ResponsiveContainer, BarChart, Bar, LineChart, Line } from 'recharts';
+
+const data = [
+	{
+		name: 'Page A',
+		uv: 4000,
+		pv: 2400,
+		amt: 2400
+	},
+	{
+		name: 'Page B',
+		uv: 3000,
+		pv: 1398,
+		amt: 2210
+	},
+	{
+		name: 'Page C',
+		uv: 5000,
+		pv: 9800,
+		amt: 2290
+	},
+	{
+		name: 'Page D',
+		uv: 2780,
+		pv: 3908,
+		amt: 2000
+	},
+	{
+		name: 'Page E',
+		uv: 1890,
+		pv: 4800,
+		amt: 2181
+	},
+	{
+		name: 'Page F',
+		uv: 7390,
+		pv: 3800,
+		amt: 2500
+	},
+	{
+		name: 'Page G',
+		uv: 3490,
+		pv: 4300,
+		amt: 2100
+	},
+	{
+		name: 'Page H',
+		uv: 2490,
+		pv: 8300,
+		amt: 2100
+	},
+	{
+		name: 'Page I',
+		uv: 6490,
+		pv: 5300,
+		amt: 2100
+	},
+	{
+		name: 'Page J',
+		uv: 3490,
+		pv: 3300,
+		amt: 2100
+	},
+	{
+		name: 'Page K',
+		uv: 2490,
+		pv: 6300,
+		amt: 2100
+	},
+	{
+		name: 'Page I',
+		uv: 6490,
+		pv: 4300,
+		amt: 2100
+	}
+];
 
 const DashboardView = () => {
 	return (
 		<div className='dashboard'>
 			<Row gutter={[16, 16]}>
-				<Col span={12}>
+				<Col xs={24} sm={12} md={8} lg={8} xl={6}>
 					<Card>
-						<Statistic title='Total schools' value={8} />
+						<Statistic title='Total Sales' value={'126,560'} />
+						<ResponsiveContainer width='100%' height={40}>
+							<AreaChart
+								width={200}
+								height={60}
+								data={data}
+								margin={{
+									top: 5,
+									right: 0,
+									left: 0,
+									bottom: 5
+								}}
+							>
+								<Area
+									type='monotone'
+									dataKey='uv'
+									stroke='#975FE4'
+									fill='#975FE4'
+								/>
+							</AreaChart>
+						</ResponsiveContainer>
+						<Divider style={{ margin: '9px 0px' }} />
+						<span>Daily sales 12,423</span>
 					</Card>
 				</Col>
-				<Col span={12}>
+				<Col xs={24} sm={12} md={8} lg={8} xl={6}>
 					<Card>
-						<Statistic title='Total programs' value={16} />
+						<Statistic title='Visits' value={'8,846'} />
+						<ResponsiveContainer width='100%' height={40}>
+							<BarChart width={150} height={40} data={data}>
+								<Bar dataKey='uv' fill='#3AA0FF' />
+							</BarChart>
+						</ResponsiveContainer>
+						<Divider style={{ margin: '9px 0px' }} />
+						<span>Daily visits 1,234</span>
 					</Card>
 				</Col>
-				<Col span={12}>
+				<Col xs={24} sm={12} md={8} lg={8} xl={6}>
 					<Card>
-						<Statistic title='Total institutions' value={32} />
+						<Statistic title='Payments' value={'6,560'} />
+						<ResponsiveContainer width='100%' height={40}>
+							<LineChart width={300} height={100} data={data}>
+								<Line
+									type='monotone'
+									dataKey='pv'
+									stroke='#37ed00'
+									strokeWidth={2}
+								/>
+							</LineChart>
+						</ResponsiveContainer>
+						<Divider style={{ margin: '9px 0px' }} />
+						<span>Conversion Rate 60%</span>
 					</Card>
 				</Col>
-				<Col span={12}>
+				<Col xs={24} sm={12} md={8} lg={8} xl={6}>
 					<Card>
-						<Statistic title='Total subscriptions' value={64} />
+						<Statistic title='Operational Effect' value={'78%'} />
+						<div style={{ width: '100%', height: '40px', padding: '16px 0px' }}>
+							<div
+								style={{ width: '78%', height: '100%', background: '#13C2C2' }}
+							></div>
+						</div>
+						<Divider style={{ margin: '9px 0px' }} />
+						<span>WoW Change 12%</span>
 					</Card>
 				</Col>
 			</Row>
